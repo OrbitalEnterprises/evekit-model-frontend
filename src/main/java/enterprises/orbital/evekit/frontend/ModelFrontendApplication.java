eaff2254d8dac043cd3f19766fcffab823ff9318
deadlybulb <deadlybulb@orbital.enterprises>
2018-01-29 22:52:05 -0500

    transform BigDecimal to double for swagger config

--- src/main/java/enterprises/orbital/evekit/frontend/ModelFrontendApplication.java
+++ src/main/java/enterprises/orbital/evekit/frontend/ModelFrontendApplication.java
@@ -1,11 +1,17 @@
 package enterprises.orbital.evekit.frontend;
 
 import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.math.BigDecimal;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 
 import javax.ws.rs.core.Application;
 
+import com.fasterxml.jackson.databind.JavaType;
 import enterprises.orbital.base.OrbitalProperties;
 import enterprises.orbital.base.PersistentProperty;
 import enterprises.orbital.db.DBPropertyProvider;
@@ -14,23 +20,69 @@ import enterprises.orbital.evekit.ws.common.ModelAccessKeyWS;
 import enterprises.orbital.evekit.ws.common.ModelCommonWS;
 import enterprises.orbital.evekit.ws.common.ModelMetaWS;
 import enterprises.orbital.evekit.ws.corporation.ModelCorporationWS;
+import io.swagger.converter.ModelConverter;
+import io.swagger.converter.ModelConverterContext;
+import io.swagger.converter.ModelConverters;
+import io.swagger.jackson.AbstractModelConverter;
+import io.swagger.models.Model;
+import io.swagger.models.ModelImpl;
+import io.swagger.models.properties.DoubleProperty;
+import io.swagger.models.properties.Property;
+import io.swagger.util.Json;
 
 public class ModelFrontendApplication extends Application {
   // Property which holds the name of the persistence unit for properties
   public static final String PROP_PROPERTIES_PU = "enterprises.orbital.evekit.model-frontend.properties.persistence_unit";
+  @SuppressWarnings("unused")
   public static final String PROP_APP_PATH      = "enterprises.orbital.evekit.model-frontend.apppath";
+  @SuppressWarnings("unused")
   public static final String DEF_APP_PATH       = "http://localhost/model";
+
+  // ModelConverter which converts BigDecimal to Double instead
+  public class BigDecimalModelConverter extends AbstractModelConverter {
+
+    BigDecimalModelConverter() {
+      super(Json.mapper());
+    }
+
+    @Override
+    public Property resolveProperty(Type type, ModelConverterContext context, Annotation[] annotations, Iterator<ModelConverter> chain) {
+      JavaType _type = Json.mapper().constructType(type);
+      if (_type != null) {
+        Class<?> cls = _type.getRawClass();
+        if (BigDecimal.class.isAssignableFrom(cls)) {
+          return new DoubleProperty();
+        }
+      }
+      if (chain.hasNext()) {
+        return chain.next().resolveProperty(type, context, annotations, chain);
+      } else {
+        return null;
+      }
+    }
+
+    @Override
+    public Model resolve(Type type, ModelConverterContext context, Iterator<ModelConverter> next) {
+      if (next.hasNext()) {
+        return next.next().resolve(type, context, next);
+      } else {
+        return null;
+      }
+    }
+  }
 
   public ModelFrontendApplication() throws IOException {
     // Populate properties
     OrbitalProperties.addPropertyFile("EveKitModelFrontend.properties");
     // Sent persistence unit for properties
     PersistentProperty.setProvider(new DBPropertyProvider(OrbitalProperties.getGlobalProperty(PROP_PROPERTIES_PU)));
+    // Override BigDecimal representation
+    ModelConverters.getInstance().addConverter(new BigDecimalModelConverter());
   }
 
   @Override
   public Set<Class<?>> getClasses() {
-    Set<Class<?>> resources = new HashSet<Class<?>>();
+    Set<Class<?>> resources = new HashSet<>();
     // Model APIresources
     resources.add(ModelAccessKeyWS.class);
     resources.add(ModelMetaWS.class);
